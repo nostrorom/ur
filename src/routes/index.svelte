9510485f3380b2397c04f7cfe5bea0e878eada91
Nostro <49858211+Nostrorom@users.noreply.github.com>
2022-07-27 14:29:54 +0200

    UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,7 +25,6 @@
 	};
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-lg text-orange-700">Welcome to UR</h1>
 
 <canvas id="canvas" />
