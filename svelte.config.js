44b50987c4ce6d44ec473ba8f1a4ba11df70ab27
Nostro <49858211+Nostrorom@users.noreply.github.com>
2022-07-27 14:22:09 +0200

    upgrade packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,16 +9,7 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter(),
-		vite: {
-			resolve: {
-				alias: {
-					$lib: resolve('./src/lib'),
-					$shared: resolve('./src/shared'),
-					$types: resolve('./src/types')
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
